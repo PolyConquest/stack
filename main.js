a96ff65575eb9e0c9397a1974217f239e8db50a0
Sebastien-Gineste <47150778+Sebastien-Gineste@users.noreply.github.com>
2022-04-29 13:10:21 +0200

    update game

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("ok")
+// -------------------------------
+// Fichier généré automatiquement par polyconquest :
+// Veuillez utiliser ce fichier pour envoyer votre code
+// -------------------------------
+// Reads the number of disks in the puzzle
+//const numberOfDisks = parseInt(readline());
+
+// Moves the smallest disk from the source-tower 0 (left) to the target-tower 1 (middle)
+console.log('stack 0');
+console.log("stack 1")
